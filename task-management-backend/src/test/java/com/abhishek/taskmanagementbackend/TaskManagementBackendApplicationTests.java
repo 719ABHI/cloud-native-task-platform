fc130e7064dab7a3b6961dfb227f6319e1c0aa21
Abhishek Angadi <abhishekangadi2004@gmail.com>
2026-07-06 00:10:51 +0530

    feat: initialize Spring Boot backend with PostgreSQL

---
+++ task-management-backend/src/test/java/com/abhishek/taskmanagementbackend/TaskManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.taskmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
